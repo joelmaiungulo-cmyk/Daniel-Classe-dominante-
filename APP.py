066703328cae8a7272f9249d37092ecb42f8b098
joelmaiungulo-cmyk <joelmaiungulo@gmail.com>
2025-10-18 19:34:43 +0100

    Create APP.py

---
+++ APP.py
@@ -0,0 +1,7 @@
+from flask import Flask import os
+app = Flask (__name__)
+@app.route('/') def home():
+  return ✈️ Daniel classe dominante Bot está online e pronto para operar 
+if __name__ == '__main__':
+  port = int(os.environ.get(' PORT', 5000)
+             app.run( host='0.0.0.0'.pory=port)
